38905735142c287a0bc7b4f6829f9049c9a60418
mattHG70 <matze.huebscher@gmail.com>
2024-05-16 21:46:40 +0200

    inital version of batch job script

--- scripts/transfer_learning/run_tl_features.sh
+++ scripts/transfer_learning/run_tl_features.sh
@@ -1,26 +1,30 @@
 #!/bin/bash
-# UGE PARAMETERS
-#$ -N slc
-#$ -pe smp 1
-#$ -binding linear:1
-#$ -cwd
-#$ -S /bin/bash
-#$ -l m_mem_free=64G
-#$ -l h_rt=24:00:00
-#$ -l gpu_card=1
-#$ -j y
+# Slurm PARAMETERS
+#SBATCH --job-name=team10_tl
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=64g
+#SBATCH --account=SIADS 696-012
+#SBATCH --mail-user mhuebsch@umich.edu
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
+#SBATCH --time=01:00:00
+#SBATCH --output=/home/%u/%x-%j.log
+#SBATCH --error=/home/%u/error-%x-%j.log
 
-# UGE PARAMETERS END
 # run bashrc
-source /home/ghisuma1/.bashrc
-cd /da/isld/ghisuma1/shared/cm_pheno/jlifesci_cm
-conda activate cm_pheno_v2
+source /home/mhuebsh/.bashrc
+cd /
+module load tensorflow cuda cudnn
+conda activate ms2
 
 # some fancy logging
 START=`date +%s`; STARTDATE=`date`;
 echo [INFO] [$START] [$STARTDATE] [$$] [$JOB_ID] Starting the workflow
 echo [INFO] [$START] [$STARTDATE] [$$] [$JOB_ID] We got the following cores: $CUDA_VISIBLE_DEVICES
-python create_tl_features.py -dev cuda -image_list linux_imageList -channels "ACTIN,ALPHA_ACTININ,NUCLEI"
+python create_tl_features.py -dev cuda -image_list linux_imageList -channels "DAPI,Tubulin,Actin"
 
 EXITCODE=$?
 # some fancy logging
