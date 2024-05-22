f112240bf100f5070e11cf162f0ae8f8e8689ad7
Matthias Huebscher <mhuebsch@gl-login2.arc-ts.umich.edu>
2024-05-22 06:50:14 -0400

    run full embedding creation

--- scripts/transfer_learning/run_tl_features.sh
+++ scripts/transfer_learning/run_tl_features.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=team10_tl
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=4g
+#SBATCH --mem-per-cpu=8g
 #SBATCH --account=siads696s24_class
 #SBATCH --mail-user mhuebsch@umich.edu
 #SBATCH --mail-type=BEGIN,END,FAIL
@@ -12,7 +12,7 @@
 #SBATCH --ntasks-per-gpu=1
 #SBATCH --gpus=1
 #SBATCH --gpus-per-node=1
-#SBATCH --time=00:10:00
+#SBATCH --time=06:30:00
 #SBATCH --output=/home/%u/siads696/%x-%j.log
 #SBATCH --error=/home/%u/siads696/error-%x-%j.log
 
@@ -29,8 +29,8 @@ START=`date +%s`; STARTDATE=`date`;
 echo [INFO] [$START] [$STARTDATE] [$$] [$JOB_ID] Starting the workflow
 echo [INFO] [$START] [$STARTDATE] [$$] [$JOB_ID] We got the following cores: $CUDA_VISIBLE_DEVICES
 python create_tl_features.py -dev cuda \
-    -image_list /home/mhuebsch/SIADS696_Milestone2/data/BBBC021_v1_final_test \
-    -outdir /home/mhuebsch/siads696/data  \
+	-image_list /home/mhuebsch/SIADS696_Milestone2/data/BBBC021_v1_final \
+	-outdir /home/mhuebsch/siads696/data  \
 	-channels "DAPI,Tubulin,Actin"
 
 EXITCODE=$?
