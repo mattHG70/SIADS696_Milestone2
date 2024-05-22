a6b624364a94a0e438102e92f3be8cb62ff6739d
mattHG70 <matze.huebscher@gmail.com>
2024-05-22 13:35:50 +0200

    get image zip file and unzip

---
+++ scripts/data/get_image_files.sh
@@ -0,0 +1,165 @@
+wget https://data.broadinstitute.org/bbbc/BBBC021/BBBC021_v1_images_Week1_22123.zip
+unzip -q BBBC021_v1_images_Week1_22123.zip
+rm BBBC021_v1_images_Week1_22123.zip
+wget https://data.broadinstitute.org/bbbc/BBBC021/BBBC021_v1_images_Week1_22141.zip
+unzip -q BBBC021_v1_images_Week1_22141.zip
+rm BBBC021_v1_images_Week1_22141.zip
+wget https://data.broadinstitute.org/bbbc/BBBC021/BBBC021_v1_images_Week1_22161.zip
+unzip -q BBBC021_v1_images_Week1_22161.zip
+rm BBBC021_v1_images_Week1_22161.zip
+wget https://data.broadinstitute.org/bbbc/BBBC021/BBBC021_v1_images_Week1_22361.zip
+unzip -q BBBC021_v1_images_Week1_22361.zip
+rm BBBC021_v1_images_Week1_22361.zip
+wget https://data.broadinstitute.org/bbbc/BBBC021/BBBC021_v1_images_Week1_22381.zip
+unzip -q BBBC021_v1_images_Week1_22381.zip
+rm BBBC021_v1_images_Week1_22381.zip
+wget https://data.broadinstitute.org/bbbc/BBBC021/BBBC021_v1_images_Week1_22401.zip
+unzip -q BBBC021_v1_images_Week1_22401.zip
+rm BBBC021_v1_images_Week1_22401.zip
+wget https://data.broadinstitute.org/bbbc/BBBC021/BBBC021_v1_images_Week2_24121.zip
+unzip -q BBBC021_v1_images_Week2_24121.zip
+rm BBBC021_v1_images_Week2_24121.zip
+wget https://data.broadinstitute.org/bbbc/BBBC021/BBBC021_v1_images_Week2_24141.zip
+unzip -q BBBC021_v1_images_Week2_24141.zip
+rm BBBC021_v1_images_Week2_24141.zip
+wget https://data.broadinstitute.org/bbbc/BBBC021/BBBC021_v1_images_Week2_24161.zip
+unzip -q BBBC021_v1_images_Week2_24161.zip
+rm BBBC021_v1_images_Week2_24161.zip
+wget https://data.broadinstitute.org/bbbc/BBBC021/BBBC021_v1_images_Week2_24361.zip
+unzip -q BBBC021_v1_images_Week2_24361.zip
+rm BBBC021_v1_images_Week2_24361.zip
+wget https://data.broadinstitute.org/bbbc/BBBC021/BBBC021_v1_images_Week2_24381.zip
+unzip -q BBBC021_v1_images_Week2_24381.zip
+rm BBBC021_v1_images_Week2_24381.zip
+wget https://data.broadinstitute.org/bbbc/BBBC021/BBBC021_v1_images_Week2_24401.zip
+unzip -q BBBC021_v1_images_Week2_24401.zip
+rm BBBC021_v1_images_Week2_24401.zip
+wget https://data.broadinstitute.org/bbbc/BBBC021/BBBC021_v1_images_Week3_25421.zip
+unzip -q BBBC021_v1_images_Week3_25421.zip
+rm BBBC021_v1_images_Week3_25421.zip
+wget https://data.broadinstitute.org/bbbc/BBBC021/BBBC021_v1_images_Week3_25441.zip
+unzip -q BBBC021_v1_images_Week3_25441.zip
+rm BBBC021_v1_images_Week3_25441.zip
+wget https://data.broadinstitute.org/bbbc/BBBC021/BBBC021_v1_images_Week3_25461.zip
+unzip -q BBBC021_v1_images_Week3_25461.zip
+rm BBBC021_v1_images_Week3_25461.zip
+wget https://data.broadinstitute.org/bbbc/BBBC021/BBBC021_v1_images_Week3_25681.zip
+unzip -q BBBC021_v1_images_Week3_25681.zip
+rm BBBC021_v1_images_Week3_25681.zip
+wget https://data.broadinstitute.org/bbbc/BBBC021/BBBC021_v1_images_Week3_25701.zip
+unzip -q BBBC021_v1_images_Week3_25701.zip
+rm BBBC021_v1_images_Week3_25701.zip
+wget https://data.broadinstitute.org/bbbc/BBBC021/BBBC021_v1_images_Week3_25721.zip
+unzip -q BBBC021_v1_images_Week3_25721.zip
+rm BBBC021_v1_images_Week3_25721.zip
+wget https://data.broadinstitute.org/bbbc/BBBC021/BBBC021_v1_images_Week4_27481.zip
+unzip -q BBBC021_v1_images_Week4_27481.zip
+rm BBBC021_v1_images_Week4_27481.zip
+wget https://data.broadinstitute.org/bbbc/BBBC021/BBBC021_v1_images_Week4_27521.zip
+unzip -q BBBC021_v1_images_Week4_27521.zip
+rm BBBC021_v1_images_Week4_27521.zip
+wget https://data.broadinstitute.org/bbbc/BBBC021/BBBC021_v1_images_Week4_27542.zip
+unzip -q BBBC021_v1_images_Week4_27542.zip
+rm BBBC021_v1_images_Week4_27542.zip
+wget https://data.broadinstitute.org/bbbc/BBBC021/BBBC021_v1_images_Week4_27801.zip
+unzip -q BBBC021_v1_images_Week4_27801.zip
+rm BBBC021_v1_images_Week4_27801.zip
+wget https://data.broadinstitute.org/bbbc/BBBC021/BBBC021_v1_images_Week4_27821.zip
+unzip -q BBBC021_v1_images_Week4_27821.zip
+rm BBBC021_v1_images_Week4_27821.zip
+wget https://data.broadinstitute.org/bbbc/BBBC021/BBBC021_v1_images_Week4_27861.zip
+unzip -q BBBC021_v1_images_Week4_27861.zip
+rm BBBC021_v1_images_Week4_27861.zip
+wget https://data.broadinstitute.org/bbbc/BBBC021/BBBC021_v1_images_Week5_28901.zip
+unzip -q BBBC021_v1_images_Week5_28901.zip
+rm BBBC021_v1_images_Week5_28901.zip
+wget https://data.broadinstitute.org/bbbc/BBBC021/BBBC021_v1_images_Week5_28921.zip
+unzip -q BBBC021_v1_images_Week5_28921.zip
+rm BBBC021_v1_images_Week5_28921.zip
+wget https://data.broadinstitute.org/bbbc/BBBC021/BBBC021_v1_images_Week5_28961.zip
+unzip -q BBBC021_v1_images_Week5_28961.zip
+rm BBBC021_v1_images_Week5_28961.zip
+wget https://data.broadinstitute.org/bbbc/BBBC021/BBBC021_v1_images_Week5_29301.zip
+unzip -q BBBC021_v1_images_Week5_29301.zip
+rm BBBC021_v1_images_Week5_29301.zip
+wget https://data.broadinstitute.org/bbbc/BBBC021/BBBC021_v1_images_Week5_29321.zip
+unzip -q BBBC021_v1_images_Week5_29321.zip
+rm BBBC021_v1_images_Week5_29321.zip
+wget https://data.broadinstitute.org/bbbc/BBBC021/BBBC021_v1_images_Week5_29341.zip
+unzip -q BBBC021_v1_images_Week5_29341.zip
+rm BBBC021_v1_images_Week5_29341.zip
+wget https://data.broadinstitute.org/bbbc/BBBC021/BBBC021_v1_images_Week6_31641.zip
+unzip -q BBBC021_v1_images_Week6_31641.zip
+rm BBBC021_v1_images_Week6_31641.zip
+wget https://data.broadinstitute.org/bbbc/BBBC021/BBBC021_v1_images_Week6_31661.zip
+unzip -q BBBC021_v1_images_Week6_31661.zip
+rm BBBC021_v1_images_Week6_31661.zip
+wget https://data.broadinstitute.org/bbbc/BBBC021/BBBC021_v1_images_Week6_31681.zip
+unzip -q BBBC021_v1_images_Week6_31681.zip
+rm BBBC021_v1_images_Week6_31681.zip
+wget https://data.broadinstitute.org/bbbc/BBBC021/BBBC021_v1_images_Week6_32061.zip
+unzip -q BBBC021_v1_images_Week6_32061.zip
+rm BBBC021_v1_images_Week6_32061.zip
+wget https://data.broadinstitute.org/bbbc/BBBC021/BBBC021_v1_images_Week6_32121.zip
+unzip -q BBBC021_v1_images_Week6_32121.zip
+rm BBBC021_v1_images_Week6_32121.zip
+wget https://data.broadinstitute.org/bbbc/BBBC021/BBBC021_v1_images_Week6_32161.zip
+unzip -q BBBC021_v1_images_Week6_32161.zip
+rm BBBC021_v1_images_Week6_32161.zip
+wget https://data.broadinstitute.org/bbbc/BBBC021/BBBC021_v1_images_Week7_34341.zip
+unzip -q BBBC021_v1_images_Week7_34341.zip
+rm BBBC021_v1_images_Week7_34341.zip
+wget https://data.broadinstitute.org/bbbc/BBBC021/BBBC021_v1_images_Week7_34381.zip
+unzip -q BBBC021_v1_images_Week7_34381.zip
+rm BBBC021_v1_images_Week7_34381.zip
+wget https://data.broadinstitute.org/bbbc/BBBC021/BBBC021_v1_images_Week7_34641.zip
+unzip -q BBBC021_v1_images_Week7_34641.zip
+rm BBBC021_v1_images_Week7_34641.zip
+wget https://data.broadinstitute.org/bbbc/BBBC021/BBBC021_v1_images_Week7_34661.zip
+unzip -q BBBC021_v1_images_Week7_34661.zip
+rm BBBC021_v1_images_Week7_34661.zip
+wget https://data.broadinstitute.org/bbbc/BBBC021/BBBC021_v1_images_Week7_34681.zip
+unzip -q BBBC021_v1_images_Week7_34681.zip
+rm BBBC021_v1_images_Week7_34681.zip
+wget https://data.broadinstitute.org/bbbc/BBBC021/BBBC021_v1_images_Week8_38203.zip
+unzip -q BBBC021_v1_images_Week8_38203.zip
+rm BBBC021_v1_images_Week8_38203.zip
+wget https://data.broadinstitute.org/bbbc/BBBC021/BBBC021_v1_images_Week8_38221.zip
+unzip -q BBBC021_v1_images_Week8_38221.zip
+rm BBBC021_v1_images_Week8_38221.zip
+wget https://data.broadinstitute.org/bbbc/BBBC021/BBBC021_v1_images_Week8_38241.zip
+unzip -q BBBC021_v1_images_Week8_38241.zip
+rm BBBC021_v1_images_Week8_38241.zip
+wget https://data.broadinstitute.org/bbbc/BBBC021/BBBC021_v1_images_Week8_38341.zip
+unzip -q BBBC021_v1_images_Week8_38341.zip
+rm BBBC021_v1_images_Week8_38341.zip
+wget https://data.broadinstitute.org/bbbc/BBBC021/BBBC021_v1_images_Week8_38342.zip
+unzip -q BBBC021_v1_images_Week8_38342.zip
+rm BBBC021_v1_images_Week8_38342.zip
+wget https://data.broadinstitute.org/bbbc/BBBC021/BBBC021_v1_images_Week9_39206.zip
+unzip -q BBBC021_v1_images_Week9_39206.zip
+rm BBBC021_v1_images_Week9_39206.zip
+wget https://data.broadinstitute.org/bbbc/BBBC021/BBBC021_v1_images_Week9_39221.zip
+unzip -q BBBC021_v1_images_Week9_39221.zip
+rm BBBC021_v1_images_Week9_39221.zip
+wget https://data.broadinstitute.org/bbbc/BBBC021/BBBC021_v1_images_Week9_39222.zip
+unzip -q BBBC021_v1_images_Week9_39222.zip
+rm BBBC021_v1_images_Week9_39222.zip
+wget https://data.broadinstitute.org/bbbc/BBBC021/BBBC021_v1_images_Week9_39282.zip
+unzip -q BBBC021_v1_images_Week9_39282.zip
+rm BBBC021_v1_images_Week9_39282.zip
+wget https://data.broadinstitute.org/bbbc/BBBC021/BBBC021_v1_images_Week9_39283.zip
+unzip -q BBBC021_v1_images_Week9_39283.zip
+rm BBBC021_v1_images_Week9_39283.zip
+wget https://data.broadinstitute.org/bbbc/BBBC021/BBBC021_v1_images_Week9_39301.zip
+unzip -q BBBC021_v1_images_Week9_39301.zip
+rm BBBC021_v1_images_Week9_39301.zip
+wget https://data.broadinstitute.org/bbbc/BBBC021/BBBC021_v1_images_Week10_40111.zip
+unzip -q BBBC021_v1_images_Week10_40111.zip
+rm BBBC021_v1_images_Week10_40111.zip
+wget https://data.broadinstitute.org/bbbc/BBBC021/BBBC021_v1_images_Week10_40115.zip
+unzip -q BBBC021_v1_images_Week10_40115.zip
+rm BBBC021_v1_images_Week10_40115.zip
+wget https://data.broadinstitute.org/bbbc/BBBC021/BBBC021_v1_images_Week10_40119.zip
+unzip -q BBBC021_v1_images_Week10_40119.zip
+rm BBBC021_v1_images_Week10_40119.zip
