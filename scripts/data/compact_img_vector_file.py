3e8a67612f817773911b7de1ee3a18e2085948f8
Matthias Huebscher <mhuebsch@gl-login1.arc-ts.umich.edu>
2024-05-26 10:16:37 -0400

    add documentation to scripts

--- scripts/data/compact_img_vector_file.py
+++ scripts/data/compact_img_vector_file.py
@@ -1,7 +1,20 @@
+"""
+This script removes columns which are not needed for data analysis and model
+building. These columns contain mainly the image file names and path names
+such as Image_PathName_DAPI or Image_FileName_DAPI.
+In addition an additional column containing the plate number gets added. This
+plate number is used as a batch number in the PyCombat based batch correction.
+The whole experiment got done over 10 weeks on 55 plates.
+"""
 import argparse
 
 import polars as pl
 
+"""
+Command line paramters:
+- -infile: the full path of the input file created by the transfer learnings
+- -outfile: the full path of the more compact output file
+"""
 parser = argparse.ArgumentParser(description="Remove file and path columns from the embedding vector file")
 parser.add_argument('-infile', type=str, required=True, help="Input file name, full path")
 parser.add_argument('-outfile', type=str, required=True, help="Output file name, full path")
@@ -9,12 +22,22 @@ args = parser.parse_args()
 
 
 def main():
+    """
+    The main function of the script. 
+    Because of the size of the files and fact that only data manipulation
+    steps are exectuted the Polars instead of Pandas was chosen. Polars has a
+    much better performance compared to Pandas when it comes to large files.
+    """
+
+    # read in image vector file from transfer learning step
     df_pl_img_embed = pl.read_csv(args.infile)
 
+    # extract the plate number
     plate_no = df_pl_img_embed.select(
         pl.col("Metadata_Plate_DAPI").str.extract(r".+_(\d+)", group_index=1).cast(pl.Int64).alias("Metadata_PlateNumber"),
     )
-    
+
+    # add the plate number column to the dataframe
     df_pl_img_embed = pl.concat(
         [
             plate_no,
@@ -23,6 +46,8 @@ def main():
         how="horizontal",
     )
 
+    # create output dataframe containg the columns needed for analysis and
+    # model building
     out = df_pl_img_embed.select(
         pl.col("*").exclude("Image_FileName_DAPI",
                             "Image_PathName_DAPI",
@@ -35,6 +60,7 @@ def main():
                             )   
     )
 
+    # export and write the dataframe to a CSV file
     out.write_csv(args.outfile)
 
 
--- scripts/data/get_bbbc021_datasets.py
+++ scripts/data/get_bbbc021_datasets.py
@@ -1,37 +1,62 @@
+"""
+This script downloads the CSV files for the BBBC021 dataset from the 
+Broad Institue.
+Dataset URL: https://bbbc.broadinstitute.org/BBBC021
+In addtion the file path BBBC021_v1_image.csv file is set to the actual file
+path of the images because this file is the basis of transfer learning step. 
+"""
 import os
 import re
 import argparse
 
 import pandas as pd
 
+# specify the URLs and the file names
 dataset_urls =[
     ("https://data.broadinstitute.org/bbbc/BBBC021/BBBC021_v1_image.csv","BBBC021_v1_image.csv", True),
     ("https://data.broadinstitute.org/bbbc/BBBC021/BBBC021_v1_compound.csv","BBBC021_v1_compound.csv", False),
     ("https://data.broadinstitute.org/bbbc/BBBC021/BBBC021_v1_moa.csv","BBBC021_v1_moa.csv", False)
 ]
 
+
+"""
+Command line parameters:
+- -outdir: the directory in which the downloaded files are stored
+- -imgdir: the root directory of the actual image files
+"""
 parser = argparse.ArgumentParser(description="Download CSV file for BBBC021_v1 dataset")
 parser.add_argument('-outdir', type=str, required=True, help="Output directory where the files should be stored")
 parser.add_argument('-imgdir', type=str, required=True, help="Image directory for BBBC021 images")
 args = parser.parse_args()
 
+
 def change_img_path(p, imgpath):
+    """
+    Function which sets the correct path to the image.
+    """
     path = re.match(r".+\/(.+)", p)
     return os.path.join(imgpath, path.group(1))
 
+
 def main():
-    
+    """
+    The main fuction of the script.
+    The CSV files are directly read into dataframes and manipulated if needed.
+    """
     for data_file  in dataset_urls:
+        # read URL directly into dataframe
         df = pd.read_csv(data_file[0])
 
+        # change the image path in the image file to the actual path of the
+        # images
         if data_file[2]:
             img_path_cols = [c for c in df.columns if c.startswith("Image_Path")]
             for img_path in img_path_cols:
                 df[img_path] = df[img_path].apply(change_img_path, imgpath=args.imgdir)
-        
+
+        # write the dataframes to CSV files
         df.to_csv(os.path.join(args.outdir, data_file[1]), index=False)
-    
-    print("files written")
+
     
 if __name__=="__main__":
     main()
